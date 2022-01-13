30ac71a5b1200e6ec798a339fc518829a50cf526
Miwka <97275239+albertu4@users.noreply.github.com>
2022-01-13 18:03:01 +0900

    made logic of lighting and changing title of button	.ViewController

--- Traffic lights/ViewController.swift
+++ Traffic lights/ViewController.swift
@@ -15,19 +15,39 @@ class ViewController: UIViewController {
     
     @IBOutlet var startButton: UIButton!
     
+    var lightColor = "red"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         redLight.layer.cornerRadius = 90
         yellowLight.layer.cornerRadius = 90
         greenLight.layer.cornerRadius = 90
-        
         startButton.layer.cornerRadius = 10
         
         redLight.alpha = 0.3
         yellowLight.alpha = 0.3
         greenLight.alpha = 0.3
     }
-
-
+    
+    @IBAction func changeLightColors() {
+        
+        if lightColor == "red" {
+            greenLight.alpha = 0.3
+            redLight.alpha = 1
+            lightColor = "yellow"
+        } else if lightColor == "yellow" {
+            redLight.alpha = 0.3
+            yellowLight.alpha = 1
+            lightColor = ""
+        } else {
+            yellowLight.alpha = 0.3
+            greenLight.alpha = 1
+            lightColor = "red"
+        }
+        
+        startButton.setTitle("Next", for: .normal)
+    }
+    
 }
 
