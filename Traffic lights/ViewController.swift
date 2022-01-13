e16398eae8379beea9a35dc902ad0b120eb0a69b
Miwka <97275239+albertu4@users.noreply.github.com>
2022-01-13 17:21:03 +0900

    rounded corners and set alpha

--- Traffic lights/ViewController.swift
+++ Traffic lights/ViewController.swift
@@ -9,9 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    
+    @IBOutlet var startButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = 90
+        yellowLight.layer.cornerRadius = 90
+        greenLight.layer.cornerRadius = 90
+        
+        startButton.layer.cornerRadius = 10
+        
+        redLight.alpha = 0.3
+        yellowLight.alpha = 0.3
+        greenLight.alpha = 0.3
     }
 
 
